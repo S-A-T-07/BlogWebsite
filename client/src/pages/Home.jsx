54c4c9fb034e8188ed468ae141e16055d746d433
Soumya Annie Thomas <soumyaannie07@gmail.com>
2025-02-01 03:11:54 +0530

    logo

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 
-import Logo from "../assets/logo.png";
+import Logo from "../assets/Logo.png";
 
 export default function Home() {
   return (
