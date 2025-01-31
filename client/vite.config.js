2367ce597af26aef9317b798834fad72ad18b65a
Soumya Annie Thomas <soumyaannie07@gmail.com>
2025-02-01 03:07:05 +0530

    package

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,25 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "tailwindcss"; // Import Tailwind CSS
+import autoprefixer from "autoprefixer"; // Optional: For autoprefixing CSS
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
+      "/api": {
+        target: "http://localhost:3000",
         secure: false,
       },
     },
   },
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(), // Add Tailwind CSS
+        autoprefixer(), // Optional: Add autoprefixer for better browser compatibility
+      ],
+    },
+  },
 });
